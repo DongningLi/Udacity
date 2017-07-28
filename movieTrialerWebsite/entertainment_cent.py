37283c55c8b357a35143abc9be70bf7f7ae429d4
Dongning Li <domeihappy@163.com>
2017-07-28 18:31:51 +0800

    update the style of code  to conform to PEP8 style guide

--- movieTrialerWebsite/entertainment_cent.py
+++ movieTrialerWebsite/entertainment_cent.py
@@ -3,28 +3,35 @@ import fresh_tomatoes
 
 '''create threee medium'''
 toy_story = media.Movie("Toy Story",
-                        "A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him "
+                        "A cowboy doll is profoundly threatened and jealous"
+                        "when a new spaceman figure supplants him"
                         "as top toy in a boy's room.",
                         "https://images-na.ssl-images-amazon.com/images/M"
-                        "/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@"
+                        "/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJ"
+                        "kXkEyXkFqcGdeQXVyNDQ2OTk4MzI@"
                         "._V1_SY1000_SX670_AL.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc",
                         "1995")
 
 friendsEpisode1 = media.TVShow("Friends",
-                               "Follows the personal and professional lives of six 20 to 30-something-year-old friends"
-                               " living in Manhattan.",
-                               "https://images-na.ssl-images-amazon.com/images/M"
-                               "/MV5BMTg4NzEyNzQ5OF5BMl5BanBnXkFtZTYwNTY3NDg4._V1._CR24,0,293,443_.jpg",
+                               "Follows the personal and professional lives"
+                               "of six 20 to 30-something-year-old friends"
+                               "living in Manhattan.",
+                               "https://images-na.ssl-images-amazon.com/images"
+                               "/M/MV5BMTg4NzEyNzQ5OF5BMl5BanBnXkFtZTYwNT"
+                               "Y3NDg4._V1._CR24,0,293,443_.jpg",
                                "https://www.youtube.com/watch?v=hDNNmeeJs1Q",
                                "Episode 1")
 
 eatPrayLove = media.Movie("Eat Pray Love",
-                          "A married woman realizes how unhappy her marriage really is,"
-                          " and that her life needs to go in a different direction. After a painful divorce,"
-                          " she takes off on a round-the-world journey to 'find herself'.",
+                          "A married woman realizes how unhappy her marriage"
+                          "really is, and that her life needs to go in a"
+                          "different direction. After a painful divorce,"
+                          "she takes off on a round-the-world journey"
+                          "to 'find herself'.",
                           "https://images-na.ssl-images-amazon.com/images/M"
-                          "/MV5BMTY5NDkyNzkyM15BMl5BanBnXkFtZTcwNDQyNDk0Mw@@._V1_.jpg",
+                          "/MV5BMTY5NDkyNzkyM15BMl5BanBnXkFtZTcwNDQyNDk0Mw@@"
+                          "._V1_.jpg",
                           "https://www.youtube.com/watch?v=mjay5vgIwt4",
                           "2010")
 
